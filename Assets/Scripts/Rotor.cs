e2ac23c86a59f0a354132ab1e423bb798eba8c8b
Vladislav Andresov <vv.andresov@gmail.com>
2020-10-23 19:28:23 +0300

    Implement panel rotation by touching it's pieces
    Signed-off-by: Vladislav Andresov <vv.andresov@gmail.com>

---
+++ Assets/Scripts/Rotor.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Rotor : MonoBehaviour
+{
+    [SerializeField] private List<Vector3> Axes;
+
+    private static float RotationAngle = 90f;
+
+    private Transform RubiksCube;
+
+    void Start()
+    {
+        RubiksCube = transform.parent;
+    }
+
+    public void Rotate(Quaternion rotation)
+    {
+        Vector3 direction;
+        float angle;
+        rotation.ToAngleAxis(out angle, out direction);
+
+        var neighbors = GetNeighbors(direction);
+
+        Assert.IsNotNull(neighbors);
+
+        foreach (Transform cube in neighbors)
+        {
+            cube.parent = transform;
+        }
+
+        transform.RotateAround(transform.position, direction, angle);
+
+        foreach (Transform cube in neighbors)
+        {
+            cube.parent = RubiksCube;
+        }
+
+        Assert.IsNotNull(GetNeighbors(direction));
+    }
+
+    public bool GetRotation(Transform from, Transform to, out Quaternion rotation)
+    {
+        rotation = Quaternion.identity;
+
+        foreach (var axis in Axes)
+        {
+            var neighbors = GetNeighbors(axis);
+
+            if (neighbors is null)
+            {
+                continue;
+            }
+
+            var isFromNeighborFound = false;
+            var isToNeighborFound = false;
+
+            foreach (var neighbor in neighbors)
+            {
+                isFromNeighborFound = isFromNeighborFound || from == neighbor.transform;
+                isToNeighborFound = isToNeighborFound || to == neighbor.transform;
+            }
+
+            if (isFromNeighborFound && isToNeighborFound)
+            {
+                var rotationAxis = Vector3.Cross(from.position - transform.position,
+                                                 to.position - transform.position);
+
+                rotation = Quaternion.AngleAxis(RotationAngle, rotationAxis);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private List<Transform> GetNeighbors(Vector3 axis)
+    {
+        var neighbors = new List<Transform>();
+        var axisPlane = new Plane(axis, transform.position);
+        
+        foreach (Transform piece in RubiksCube)
+        {
+            if (piece.transform.transform != transform &&
+                Mathf.Abs(axisPlane.GetDistanceToPoint(piece.transform.position)) < Vector3.kEpsilon)
+            {
+                neighbors.Add(piece);
+            }
+        }
+
+        if (neighbors.Count != 8)
+        {
+            return null;
+        }
+
+        return neighbors;
+    }
+}
---
+++ Assets/Scripts/RubiksCube.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class RubiksCube : MonoBehaviour
+{
+    [SerializeField] float EdgeLength; // TODO: remove it
+    [SerializeField] List<Rotor> Rotors;
+
+    private RotationInfo RotationInfo;
+
+    private bool isMouseButtonDown0 = false;
+
+    private Vector3 MousePositon;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MousePositon = Input.mousePosition;
+            StartScreenTracking(MousePositon);
+            isMouseButtonDown0 = true;
+            return;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            StopScreenTracking(Input.mousePosition);
+            isMouseButtonDown0 = false;
+            return;
+
+        }
+
+        if (isMouseButtonDown0)
+        {
+            if (Vector3.Distance(Input.mousePosition, MousePositon) < 1f)
+            {
+                return;
+            }
+
+            MousePositon = Input.mousePosition;
+
+            ContinueScreenTracking(MousePositon);
+        }
+    }
+
+    private void StartScreenTracking(Vector3 screenPosition)
+    {
+        if (!(RotationInfo is null))
+        {
+            StopScreenTracking(screenPosition);
+        }
+
+        RotationInfo = new RotationInfo();
+
+        var cameraRay = Camera.main.ScreenPointToRay(screenPosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(cameraRay, out hit))
+        {
+            RotationInfo.From = new RotationInfo.CubeTouch {
+                Piece =  hit.transform,
+                Point = hit.point
+            };
+        }
+    }
+
+    private void ContinueScreenTracking(Vector3 screenPosition)
+    {
+        if (RotationInfo.From is null)
+        {
+            return;
+        }
+    }
+
+    private void StopScreenTracking(Vector3 screenPosition)
+    {
+        if (RotationInfo is null)
+        {
+            return;
+        }
+
+        var cameraRay = Camera.main.ScreenPointToRay(screenPosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(cameraRay, out hit))
+        {
+            RotationInfo.To = new RotationInfo.CubeTouch {
+                Piece =  hit.transform,
+                Point = hit.point
+            };
+        }
+        
+        ProcessRotationInfo(RotationInfo);
+
+        RotationInfo = null;
+    }
+
+    private void ProcessRotationInfo(RotationInfo rotationInfo)
+    {
+        if (rotationInfo.From != null && rotationInfo.To != null)
+        {
+            RotateRotor(rotationInfo.From, rotationInfo.To);
+        }
+    }
+
+    private void RotateRotor(RotationInfo.CubeTouch from, RotationInfo.CubeTouch to)
+    {
+        var rotorAndRotation = FindRotorAndRotation(from, to);
+        
+        if (rotorAndRotation is null)
+        {
+            return;
+        }
+
+        var rotor = rotorAndRotation.Item1;
+        var rotation = rotorAndRotation.Item2;
+
+        rotor.Rotate(rotation);
+    }
+
+    private Tuple<Rotor, Quaternion> FindRotorAndRotation(RotationInfo.CubeTouch from, RotationInfo.CubeTouch to)
+    {
+        foreach (var rotor in Rotors)
+        {
+            var rotation = Quaternion.identity;
+
+            if (rotor.GetRotation(from.Piece, to.Piece, out rotation) &&
+                IsRotationFound(rotation, from.Point, to.Point)
+            ) {
+                
+                return new Tuple<Rotor, Quaternion>(rotor, rotation);
+            }
+        }
+
+        return null;
+    }
+
+    private bool IsRotationFound(Quaternion rotation, Vector3 pointFrom, Vector3 pointTo)
+    {
+        Vector3 dir;
+        float angle;
+        rotation.ToAngleAxis(out angle, out dir);
+
+        if (Vector3.Distance(dir, transform.up) < Vector3.kEpsilon || Vector3.Distance(dir, -transform.up) < Vector3.kEpsilon) {
+            return !(IsPointOnAxisPerpendicularCubeFaces(pointFrom, transform.up) ||
+                     IsPointOnAxisPerpendicularCubeFaces(pointTo, transform.up));
+        } else if (Vector3.Distance(dir, transform.right) < Vector3.kEpsilon || Vector3.Distance(dir, -transform.right) < Vector3.kEpsilon) {
+            return !(IsPointOnAxisPerpendicularCubeFaces(pointFrom, transform.right) ||
+                     IsPointOnAxisPerpendicularCubeFaces(pointTo, transform.right));
+        } else if (Vector3.Distance(dir, transform.forward) < Vector3.kEpsilon || Vector3.Distance(dir, -transform.forward) < Vector3.kEpsilon) {
+            return !(IsPointOnAxisPerpendicularCubeFaces(pointFrom, transform.forward) ||
+                     IsPointOnAxisPerpendicularCubeFaces(pointTo, transform.forward));
+        }
+
+        Assert.IsTrue(false);
+
+        return false;
+    }
+
+    private bool IsPointOnAxisPerpendicularCubeFaces(Vector3 point, Vector3 axis)
+    {
+        if (Mathf.Abs(new Plane(axis, transform.position + 0.5f * axis * EdgeLength).GetDistanceToPoint(point)) < Vector3.kEpsilon)
+        {
+            return true;
+        }
+
+        if (Mathf.Abs(new Plane(axis, transform.position - 0.5f * axis * EdgeLength).GetDistanceToPoint(point)) < Vector3.kEpsilon)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+class RotationInfo
+{
+    public class CubeTouch
+    {
+        public Transform Piece;
+        public Vector3 Point;
+    }
+
+    public CubeTouch From = null;
+    public CubeTouch To = null;
+}
